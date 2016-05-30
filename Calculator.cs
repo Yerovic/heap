9c7b638a7138ca669a47b65daf25d0681d2a61b8
Yerovic <Yerovic@users.noreply.github.com>
2016-05-30 03:08:13 +0300

    Add 2 .cs files

---
+++ Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace Test3
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Sort the values (v1 <= v2 <= v3)
+        /// </summary>
+        public static void SortValues(ref double v1, ref double v2, ref double v3)
+        {
+            if (v1 > v2)
+                Swap(ref v1, ref v2);
+            if (v2 > v3)
+                Swap(ref v2, ref v3);
+            if (v1 > v2)
+                Swap(ref v1, ref v2);
+        }
+
+
+        /// <summary>
+        /// Exchange the values
+        /// </summary>
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T tmp = lhs;
+            lhs = rhs;
+            rhs = tmp;
+        }
+
+
+        /// <summary>
+        /// Calculate the area of a triangle whose sides have lengths a, b, and c.
+        /// </summary>
+        public static double TriangleArea(double a, double b, double c)
+        {
+            // Длина не может быть отрицательной
+            if (a < 0)
+                throw new ArgumentOutOfRangeException("a", a, "a is negative");
+            if (b < 0)
+                throw new ArgumentOutOfRangeException("a", b, "b is negative");
+            if (c < 0)
+                throw new ArgumentOutOfRangeException("a", c, "c is negative");
+
+            // Сумма 2-х сторон не может быть больше 3ей
+            if (a > b + c || b > a + c || c > a + b)
+                throw new ArgumentException("This triangle does not exist");
+
+            // Heron's formula (Numerical stability)
+            // https://en.wikipedia.org/wiki/Heron%27s_formula
+
+            // sort a >= b >= c
+            SortValues(ref c, ref b, ref a);
+
+            // Calculate area
+            double area = Math.Sqrt((a + (b + c)) * (c - (a - b)) * (c + (a - b)) * (a + (b - c))) / 4.0d;
+            return area;
+        }
+    }
+}
---
+++ CalculatorTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace Test3.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void SortValuesTest()
+        {
+            double accuracy = 0d;
+            var testDatas = new[]
+            {
+                new { v1 = 1d, v2 = 2d, v3 = 3d, v1_expected = 1d, v2_expected = 2d, v3_expected = 3d },
+                new { v1 = 1d, v2 = 3d, v3 = 2d, v1_expected = 1d, v2_expected = 2d, v3_expected = 3d },
+                new { v1 = 2d, v2 = 1d, v3 = 3d, v1_expected = 1d, v2_expected = 2d, v3_expected = 3d },
+                new { v1 = 2d, v2 = 3d, v3 = 1d, v1_expected = 1d, v2_expected = 2d, v3_expected = 3d },
+                new { v1 = 3d, v2 = 1d, v3 = 2d, v1_expected = 1d, v2_expected = 2d, v3_expected = 3d },
+                new { v1 = 3d, v2 = 2d, v3 = 1d, v1_expected = 1d, v2_expected = 2d, v3_expected = 3d },
+                new { v1 = 1d, v2 = 1d, v3 = 1d, v1_expected = 1d, v2_expected = 1d, v3_expected = 1d },
+                new { v1 = 1.000001d, v2 = 1.0000001d, v3 = 1.00000001d, v1_expected = 1.00000001d, v2_expected = 1.0000001d, v3_expected = 1.000001d },
+            };
+
+            foreach (var data in testDatas)
+            {
+                var v1 = data.v1;
+                var v2 = data.v2;
+                var v3 = data.v3;
+                Calculator.SortValues(ref v1, ref v2, ref v3);
+
+                Assert.AreEqual(data.v1_expected, v1, accuracy, "Calculator.SortValues - incorrect");
+                Assert.AreEqual(data.v2_expected, v2, accuracy, "Calculator.SortValues - incorrect");
+                Assert.AreEqual(data.v3_expected, v3, accuracy, "Calculator.SortValues - incorrect");
+            }
+        }
+
+
+        [TestMethod()]
+        public void SwapTest()
+        {
+            double accuracy = 0d;
+            var testDatas = new[]
+            {
+                new { v1 = 1.0d, v2 = 2.0d },
+                new { v1 = 22.0d, v2 = 11.0d },
+                new { v1 = 0.0d, v2 = 0.0d },
+                new { v1 = -1.0d, v2 = -2.0d },
+                new { v1 = -9999.0d, v2 = 9999.0d },
+            };
+            foreach (var data in testDatas)
+            {
+                double v1 = data.v1,
+                       v2 = data.v2,
+                       v1_expected = data.v2,
+                       v2_expected = data.v1;
+                Calculator.Swap(ref v1, ref v2);
+
+                Assert.AreEqual(v1_expected, v1, accuracy, "Calculator.Swap - incorrect");
+                Assert.AreEqual(v2_expected, v2, accuracy, "Calculator.Swap - incorrect");
+            }
+        }
+
+
+        [TestMethod()]
+        public void TriangleAreaTest()
+        {
+            double accuracy = 0.0000000000000001d;
+            var testDatas = new[]
+            {
+                new { a = 1d, b = 1d, c = 1d, area_expected = 0.43301270189221932338186158537647d },
+                new { a = 1d, b = 1d, c = Math.Sqrt(2), area_expected = 0.5d },
+                new { a = 1d, b = 1d, c = 2d, area_expected = 0d },
+                new { a = 1.1180339887498948482045868343656d, b = 1.1180339887498948482045868343656d, c = 1d, area_expected = 0.5d },
+            };
+
+            foreach (var data in testDatas)
+            {
+                var area = Calculator.TriangleArea(data.a, data.b, data.c);
+
+                Assert.AreEqual(data.area_expected, area, accuracy, "Calculator.TriangleArea - incorrect");
+            }
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TriangleAreaTest2()
+        {
+            Calculator.TriangleArea(-1d, 1d, 1d);
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TriangleAreaTest3()
+        {
+            Calculator.TriangleArea(1d, 1d, 3d);
+        }
+    }
+}
